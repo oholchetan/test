491f9a519a2d860f57396f4e6eecdcf7eb24bf34
chetan <ohol.chetan@gmail.com>
2024-01-22 14:00:36 +0530

    adding local

---
+++ src/test2/practice1.java
@@ -0,0 +1,24 @@
+package test2;
+
+public class practice1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
